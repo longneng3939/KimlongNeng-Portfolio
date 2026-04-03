9952705c4e8ebeba42752188e4c27d3339c3787d
longneng3939 <nengkimlong4@gmail.com>
2026-04-03 10:04:00 +0700

    lol

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
                 <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter uppercase leading-tight">
                   Kimlong Neng <br />
                   <span className="text-transparent bg-clip-text bg-gradient-to-r from-white to-white/30">
-                    Software Design
+                    Software Development 
                   </span>
                 </h1>
               </div>
